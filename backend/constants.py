1df61c252e7f4eeb6793a7c7fa3b8837a2344220
hertz <abhinandankumar102001@gmail.com>
2025-03-22 10:03:26 +0530

    change according to render

--- backend/constants.py
+++ backend/constants.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
+SERVER_URL = '0.0.0.0'
+PORT = os.getenv("PORT", "8000") 
 
+ENV = os.getenv("ENV", "dev")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
